91b452b6fc915e0635b1843b94d19f2895a470a2
Luis G Luna <luis.lunapa@outlook.com>
2020-03-28 21:28:08 -0600

    Created migration, factory and seeder of Usuario

--- database/seeds/UsuariosTableSeeder.php
+++ database/seeds/UsuariosTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsuariosTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UsuariosTableSeeder::class);
+        factory(\App\Usuario::class, 5)->create();
     }
 }
