e373d1b6007e8d8a7066a68d4d9f30f559a182e2
Luis G Luna <luis.lunapa@outlook.com>
2020-03-30 19:31:50 -0600

    Ready login endpoint

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('register', 'Auth\RegisterController@register');
+
+Route::post('login', 'Auth\LoginController@login');
