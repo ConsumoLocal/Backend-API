f93331db56f39255321008445989ce99a16d3070
Luis G Luna <luis.lunapa@outlook.com>
2020-04-01 23:09:29 -0600

    Completed user business relation

---
+++ database/migrations/2020_04_02_050648_add_foreign_keys_to_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_businesses', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('business_id')->references('id')->on('businesses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_businesses', function (Blueprint $table) {
+            //
+        });
+    }
+}
