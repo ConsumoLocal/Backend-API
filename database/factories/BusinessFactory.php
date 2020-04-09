cf0c2d1fcdb236798f26ed4110838cb115b58831
Luis G Luna <luis.lunapa@outlook.com>
2020-04-09 18:25:31 -0500

    Fxed business factory

--- database/factories/BusinessFactory.php
+++ database/factories/BusinessFactory.php
@@ -8,8 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(Business::class, function (Faker $faker) {
     $user = \App\User::all()->first();
     return [
-        'name' => $faker->companySuffix,
         'user_id' => $user->id,
+        'name' => $faker->companySuffix,
         'description' => $faker->text,
         'imageUrl' => $faker->url,
         'address' => $faker->address,
