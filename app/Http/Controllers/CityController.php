f100221b40270d93fbb03461b83afa1948709bc1
Luis G Luna <luis.lunapa@outlook.com>
2020-04-12 02:11:01 -0500

    requiring auth for business of a city

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+        $this->middleware('isAdmin')->only(['destroy', 'update']);
+    }
     /**
      * Display a listing of the resource.
      *
