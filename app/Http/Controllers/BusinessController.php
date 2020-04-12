b874d98fec4c0a25e01a0abd95f666273a2cf432
Luis G Luna <luis.lunapa@outlook.com>
2020-04-12 02:11:33 -0500

    Requiring auth for all operations

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -15,7 +15,7 @@ class BusinessController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api')->except('index')->except('show');
+        $this->middleware('auth:api');
         $this->middleware('isAdmin')->only(['destroy', 'update']);
     }
 
