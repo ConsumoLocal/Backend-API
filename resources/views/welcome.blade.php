5a90d2eb76f8e74145d4b9431c1841b6d4e6a1a2
Luis G Luna <luis.lunapa@outlook.com>
2020-04-25 21:47:43 -0500

    Fixed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Consumo Local - Test Server
+            Consumo Local
         </div>
                 <div class="links">
                     <a href="../">Página Principal</a>
