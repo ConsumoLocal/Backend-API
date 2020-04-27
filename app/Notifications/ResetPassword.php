9996bf148957d8fe6fc9f2b2cb9f7ffd0ec8007b
Luis G Luna <luis.lunapa@outlook.com>
2020-04-26 19:42:39 -0500

    Added message

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -46,8 +46,8 @@ class ResetPassword extends Notification
                     ->from($email)
                     ->subject('Recuperación de Contraseña')
                     ->line('First email test')
-                    ->action('Reestablecer Ahora', url('/password/reset_token/$notifiable'))
-                    ->line('El link solamente será válido durante :count minutos.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')])
+                    ->action('Reestablecer Ahora', url('/password/reset_token/' . $notifiable))
+                    ->line('El link solamente será válido durante config(\'auth.passwords.\'.config(\'auth.defaults.passwords\').\'.expire\') minutos.')
                     ->line('Gracias por ser parte de Consumo Local !');
     }
 
